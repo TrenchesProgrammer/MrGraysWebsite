b738220f9bc3f19b6634e4b55a46cbd397e00551
Heritage <ireanuoluwah@gmail.com>
2025-07-01 15:42:42 +0100

    progress on navbar

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import '../app/globals.css'
-import {Box, Flex} from '@mantine/core';
+import {Box, Flex, Text} from '@mantine/core';
 import '@mantine/core/styles.css';
 const Navbar = () => {
   return (
     <>
-        <Box bg={''} w={'100%'} h={'5rem'}>
-            <Flex>
+        {/* <Box > */}
+            <Flex h={'5rem'} justify={'space-around'} align={'center'} gap={'lg'}>
                 <Box>
                     <h2 style={{fontFamily:'Graduate'}}>MR GRAY'S</h2>
                     <h5  style={{fontFamily:'Asap condensed'}}>TATTOO STUDIO</h5>
                 </Box>
-                <Box w={'60%'} h={'100%'} >Navigation</Box>
-                <Box w={'20%'} h={'100%'}>Contact</Box>
+                <Flex  h={'65%'} w={'80%'} style={{borderRadius:'10px'}} ff={'Graduate'} gap={'20px'} align={'center'} bg={'#d9d9d9'}>
+                  <Text >Home</Text>
+                  <Text >Our Artists</Text>
+                  <Text >Navigation</Text>
+                  <Text >Contact</Text>                  
+                </Flex>
+
             </Flex>
-        </Box>
+        {/* </Box> */}
     </>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import '../app/globals.css'
 const page = () => {
   return (
     <>
       <Navbar/>
+      <div className='hero'>
+        <h1 style={{fontFamily:'Graduate'}}>Welcome to MR GRAY'S TATTOO STUDIO</h1>
+        <p style={{fontFamily:'Asap condensed'}}>Your one-stop destination for unique and personalized tattoos.</p>
+        <p style={{fontFamily:'Asap condensed'}}>Explore our portfolio, book an appointment, and get ready to wear your art with pride.</p>
+      </div>
     </>
   )
 }
